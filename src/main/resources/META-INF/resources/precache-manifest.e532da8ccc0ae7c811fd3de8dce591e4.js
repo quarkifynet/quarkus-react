6526c64a52d53d794a892251fef4479eafb9ab0c
dmi3coder <dmi3coder@gmail.com>
2020-05-26 17:20:11 +0300

    Initial commit

---
+++ src/main/resources/META-INF/resources/precache-manifest.e532da8ccc0ae7c811fd3de8dce591e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b283e6b911230537829b789e58a7c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96675381ff5976705874",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba47b2f6edf82bbb68e2",
+    "url": "/static/js/2.f00a5b13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f00a5b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96675381ff5976705874",
+    "url": "/static/js/main.2a33f903.chunk.js"
+  },
+  {
+    "revision": "608e1bc1a4ba6cf85865",
+    "url": "/static/js/runtime-main.c9a91e67.js"
+  }
+]);
